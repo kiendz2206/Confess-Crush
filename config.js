35ef285c48cef155a99d62a86ba7460b92d49350
kiendz2206 <106810144+kiendz2206@users.noreply.github.com>
2022-06-03 17:40:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Trần Khánh Linhh',
+    titleWeb: "Web tán gái",
+    introTitle: 'Là em đó',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
